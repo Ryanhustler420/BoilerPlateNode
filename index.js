4904abc64996b673b8ff964fc0ca79bab39f782d
Gaurav Gupta <gouravgupta840@gmail.com>
2018-09-26 13:50:11 +0530

    Handling HTTP GET Request

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    {id: 1, name: 'BBA'},
+    {id: 2, name: 'MBA'},
+    {id: 3, name: 'BCA'}
+];
+
+
 app.get('/',(req,res) => {
     res.send('Hello World');
 });
 
 app.get('/api/courses',(req,res) => {
-    res.send([1,2,3]);
+    res.send(courses);
 });
 
 app.get('/api/courses/:id',(req,res) => {
-    res.send(req.params);
-    // res.send(req.query);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send("Not Found");
+    res.send(course);
 });
 
 const PORT = process.env.PORT || 3000 ;
