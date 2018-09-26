33459e90be61cc33d020efda890d70b2af29a3d2
Gaurav Gupta <gouravgupta840@gmail.com>
2018-09-27 00:10:15 +0530

    added env variable to app
    set enviornoment variable using command line interface
    
    set vidly_jwtPrivateKey=mySecureKey

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const genres = require('./routes/genres');
@@ -9,6 +10,11 @@ const auth = require('./routes/auth');
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+
+if(!config.get('jwtPrivateKey')){
+    console.log('FATAL ERROR: jwtPrivateKey is not defined.');
+    process.exit(1);
+}
 
 mongoose.connect('mongodb://localhost/vidly',{ useNewUrlParser: true })
 .then(() => {console.log('Connected to MongoDB...')})
