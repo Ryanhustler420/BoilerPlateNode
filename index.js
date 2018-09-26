23a3bf6923f5b2832d8502c0f7ef5e24a00067d3
Gaurav Gupta <gouravgupta840@gmail.com>
2018-09-26 23:44:14 +0530

    added auth route for login

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const customers = require('./routes/customers');
 const rentals = require('./routes/rentals');
 const movies = require('./routes/movies');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
@@ -20,6 +21,7 @@ app.use('/api/customers',customers);
 app.use('/api/movies',movies);
 app.use('/api/rentals',rentals);
 app.use('/api/users',users);
+app.use('/api/auth',auth);
 
 const PORT = process.env.PORT || 3000 ;
 app.listen(3000, () => console.log(`Listening on port ${PORT}...`));
