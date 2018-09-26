65fc2bc3f80b5be870e44d34c0046a412584f09c
Gaurav Gupta <gouravgupta840@gmail.com>
2018-09-26 16:19:57 +0530

    Attached Mongoose to Vidly App

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const genres = require('./routes/genres');
-const Joi = require('joi');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect('mongodb://localhost/vidly',{ useNewUrlParser: true })
+.then(() => {console.log('Connected to MongoDB...')})
+.catch(err => console.error('Could not connect :( to mongodb...'));
+
 
 app.use(express.json());
 app.use('/api/genres',genres);
